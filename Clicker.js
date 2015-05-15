1ba7983c8de07bfa745aa32af9d2ae8fef2eab41
1p6 <aaapf12@gmail.com>
2015-05-15 16:39:15 -0500

    Update Clicker.js

--- Clicker.js
+++ Clicker.js
@@ -1,1 +1,11 @@
+var tickSpeed = 1;
 
+(function(){
+  var bigCookie = document.getElementById("bigCookie");
+  
+  function tick(){
+    bigCookie.click();
+    setTimeout(tick, tickSpeed);
+  }
+  tick();
+})();
